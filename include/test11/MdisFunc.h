d486b375bcfb464c49e9984a1ccf03331d456cd5
Qiu Tao <tao.qiu@megatronix.co>
2019-11-14 18:52:44 +0800

    update src

--- include/test11/MdisFunc.h
+++ include/test11/MdisFunc.h
@@ -14,27 +14,68 @@ using namespace Eigen;
 /**
  *
  *（L12为T1、T2的点间距 ...）
- * f12 = (S1*P1_x -S2*P2_x)² +(S1*P1_y -S2*P2_y)² +(S1*P1_z -S2*P2_z)² - L12² = 0;
+ * (其中m_x,m_y 为点P1,P2等在近截面(Z=1)上的偏移量,  P1_z=P2_z=1)
+ * f12 = (S1*(P1_x+m_x) -S2*(P2_x+m_x))² +(S1*(P1_y+m_y) -S2*(P2_y+m_y))² +(S1*(P1_z) -S2*(P2_z)² - L12² = 0;
  *
+ *
+ * f12 = (S1*(P1_x+m_x) -S2*(P2_x+m_x))² +(S1*(P1_y+m_y) -S2*(P2_y+m_y))² +(S1*(P1_z+m_z) -S2*(P2_z+m_z))² - L12² = 0;
  *
  * */
 class MdisFunc {
 public:
+    MdisFunc(const Vector2d &pnt1, const Vector2d &pnt2, int nIdx1, int nIdx2, double distance);
+
     void show();
 
-    double operator()(const VectorXd& x, VectorXd& grad);
 
 public:
-    int getT1(){
-        return index_T1;
+    int getS1() {
+        return m_index_S1;
     }
-    int getT2(){
-        return index_T2;
+
+    int getS2() {
+        return m_index_S2;
+    }
+
+    bool containderivative(int n) {
+        return n == m_index_S1 || n == m_index_S2 || false;
+    }
+
+    double calc_func_value(const VectorXd &x);
+
+    double calc_derivative_s1(const VectorXd &x);
+
+    double calc_derivative_s2(const VectorXd &x);
+
+    double calc_derivative_m_x(const VectorXd &x);
+
+    double calc_derivative_m_y(const VectorXd &x);
+
+    double calc_derivative_m_z(const VectorXd &x);
+
+private:
+    void updateParams(const VectorXd &x) {
+        int length = x.size();
+        m_x = x[length - 2];
+        m_y = x[length - 1];
+//        m_z = x[length - 1];
+        s1 = x[m_index_S1];
+        s2 = x[m_index_S2];
     }
 
 private:
-    int index_T1 = 0;
-    int index_T2 = 0;
+    Vector2d m_P1,m_P2;
+    int m_index_S1 = 0;
+    int m_index_S2 = 0;
+    double m_distance = 0;
+
+    double m_x = 0;
+    double m_y = 0;
+//    double m_z = 0;
+    double s1 = 0;
+    double s2 = 0;
+
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "include/TCallback.h"
 
 #include "include/TDelegate.h"
-#include "include/test11/MdisFunc.h"
+#include "include/test11/Jfunc.h"
 //#include "include/aws.h"
 
 #include <string>
@@ -1446,21 +1446,22 @@ void test_9(){
 
 
     double rx = M_PI/2;
-    double ry = 0;
-    double rz = 0;
+    double ry = M_PI/5;
+    double rz = M_PI/3;
 
 
     double scale = 2;
     Vector3d p_a (0,0,0);
     Vector3d p_b (4,0,0);
     Vector3d p_c (4,3,0);
-    Vector4d p_d (4,5,0,1);
+    Vector3d p_d (0,3,0);
+    Vector4d p_test (4,5,0,1);
 
     /**************************************************************************/
 
     Eigen::Matrix4d rotateMat,scaleMat,offsetMat,compMat;
 
-    Eigen::Vector3d offsetVec(3,1,1);
+    Eigen::Vector3d offsetVec(30,12,15);
     offsetMat.setIdentity();
     offsetMat.block<3,1>(0,3) = offsetVec;
 
@@ -1481,18 +1482,50 @@ void test_9(){
     Vector3d p_A = remapPoint(p_a,compMat);
     Vector3d p_B = remapPoint(p_b,compMat);
     Vector3d p_C = remapPoint(p_c,compMat);
+    Vector3d p_D = remapPoint(p_d,compMat);
+
+    std::cout<<"the p_A-p_D is:\n"<<p_A<<std::endl<<std::endl
+            <<p_B<<std::endl<<std::endl
+            <<p_C<<std::endl<<std::endl
+            <<p_D<<std::endl<<std::endl;
+
+
+
 
     Vector3d v_AB = p_B-p_A;
-    Vector3d v_BC = p_C-p_B;
     Vector3d v_AC = p_C-p_A;
-    double l_AB = v_AB.squaredNorm();
-    double l_BC = v_BC.squaredNorm();
-    double l_AC = v_AC.squaredNorm();
+    Vector3d v_AD = p_D-p_A;
+    Vector3d v_BC = p_C-p_B;
+    Vector3d v_BD = p_D-p_B;
+    Vector3d v_CD = p_D-p_C;
+    double l_AB = v_AB.norm();
+    double l_AC = v_AC.norm();
+    double l_AD = v_AD.norm();
+    double l_BC = v_BC.norm();
+    double l_BD = v_BD.norm();
+    double l_CD = v_CD.norm();
+
+    std::cout<<"the length is:\n"<<l_AB<<std::endl<<std::endl
+             <<l_AC<<std::endl<<std::endl
+             <<l_AD<<std::endl<<std::endl
+             <<l_BC<<std::endl<<std::endl
+             <<l_BD<<std::endl<<std::endl
+             <<l_CD<<std::endl<<std::endl;
+
+    Vector3d pro_A = p_A/p_A[2];
+    Vector3d pro_B = p_B/p_B[2];
+    Vector3d pro_C = p_C/p_C[2];
+    Vector3d pro_D = p_D/p_D[2];
+
+    std::cout<<"the pro_A-pro_D is:\n"<<pro_A<<std::endl<<std::endl
+             <<pro_B<<std::endl<<std::endl
+             <<pro_C<<std::endl<<std::endl
+             <<pro_D<<std::endl<<std::endl;
+
 
 
     std::cout<<"the compMat is:\n"<<compMat<<std::endl;
-    std::cout<<"the compMat*p_d is:\n"<<compMat*p_d<<std::endl;
-
+    std::cout<<"the compMat*p_test is:\n"<<compMat*p_test<<std::endl;
 
     std::cout<<"the rotateMat4 is:\n"<<rotateMat<<std::endl;
 
@@ -1764,6 +1797,80 @@ void testDelegate(){
     d3(1,2); //调用委托，将输出3.5
 }
 
+void test10(){
+    //calibrate front camera
+
+    //left
+    /*
+       237.5328  515.4148       1
+       155.24864 466.6159       0
+       263.27258 390.5183       3
+       359.22452 405.14447      2
+       */
+
+    //right
+    /*
+       1097.5 503.5
+       1011.5 541.5
+       903.0 425.0
+       999.5 419.5
+       */
+    std::vector<LinePairData> vecLPdata;
+    std::vector<Vector2d> points;
+    points.emplace_back(2,0.8);
+    points.emplace_back(3.22752,1.70961);
+    points.emplace_back(2.30992,1.36348);
+    points.emplace_back(1.59984,0.758242);
+
+    std::vector<Vector2d> distancePoints;
+    distancePoints.emplace_back(155.24864,720 - 466.6159);
+    distancePoints.emplace_back(237.5328,720 - 515.4148);
+    distancePoints.emplace_back(359.22452,720 - 405.14447);
+    distancePoints.emplace_back(263.27258,720 - 390.5183);
+
+    std::vector<double> vec_distances;
+    vec_distances.push_back(8);
+    vec_distances.push_back(10);
+    vec_distances.push_back(6);
+    vec_distances.push_back(6);
+    vec_distances.push_back(10);
+    vec_distances.push_back(8);
+
+    int tmp = 0;
+    for(int i=0;i<distancePoints.size();++i){
+        for(int j=i+1;j<distancePoints.size();++j){
+            LinePairData linePairData{};
+            linePairData.nIndex1 = i;
+            linePairData.nIndex2 =j;
+            linePairData.distance = vec_distances[tmp++];
+            vecLPdata.push_back(linePairData);
+        }
+    }
+
+
+/********************************************************************/
+
+    Jfunc jfunc(points,vecLPdata);
+    const int n = 6;
+    LBFGSParam<double> param;
+    LBFGSSolver<double> solver(param);
+
+    VectorXd x = VectorXd::Zero(n);
+    x[0]=15;
+    x[1]=10;
+    x[2]=15;
+    x[3]=20;
+    x[4]=0;
+    x[5]=0;
+    double fx;
+    int niter = solver.minimize(jfunc, x, fx);
+
+    std::cout << niter << " iterations" << std::endl;
+    std::cout << "x = \n" << x.transpose() << std::endl;
+    std::cout << "f(x) = " << fx << std::endl;
+    std::cout<<"end of test10...\n";
+}
+
 
 
 
@@ -1773,13 +1880,12 @@ int main() {
 //    test0_0();
 //    getData();
 
-    MdisFunc mdisfunc;
 
-    mdisfunc.show();
 //    test4();
 //    test5();
-//    test_9();
-    test6_8();
+    test_9();
+//    test10();
+//    test6_8();
 //    testDelegate();
     std::cout << "Hello, World!" << std::endl;
     return 0;
